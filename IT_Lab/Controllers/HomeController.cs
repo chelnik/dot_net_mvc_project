0831fe4b5910c8d34115d5ebf6b2446b826850e2
Vadim Chelnik <sindbadseafarer@yandex.ru>
2023-05-01 14:19:13 +0300

    Update code base

--- IT_Lab/Controllers/HomeController.cs
+++ IT_Lab/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace IT_Lab.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
